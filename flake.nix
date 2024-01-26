0121202a92098d80774e91ead27970e53f18a415
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-01-26 20:06:33 -0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "bridge-lang";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default =
+          pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.cargo
+              pkgs.rust-analyzer
+              pkgs.rustc
+            ];
+          };
+      });
+}
